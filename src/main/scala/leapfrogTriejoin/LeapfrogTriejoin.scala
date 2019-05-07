b2332ce716a965abd2ded1f059bd891341d592a3
Per Fuchs <per@shiftleft.io>
2019-05-07 18:40:44 +0200

    A predicated version of the main loop in LeapfrogTriejoin.
    No measureable performance improvement.

--- src/main/scala/leapfrogTriejoin/LeapfrogTriejoin.scala
+++ src/main/scala/leapfrogTriejoin/LeapfrogTriejoin.scala
@@ -6,15 +6,17 @@ import Predef._
 
 class LeapfrogTriejoin(trieIterators: Map[EdgeRelationship, TrieIterator], variableOrdering: Seq[String]) {
 
-  private[this] val DONE: Int = 0
-  private[this] val DOWN_ACTION: Int = 1
-  private[this]  val NEXT_ACTION: Int = 2
-  private[this] val UP_ACTION: Int = 3
-
   val allVariables = trieIterators.keys.flatMap(
     e => e.variables).toSet
 
-  private[this] val maxDepth = allVariables.size - 1
+  val maxDepth = allVariables.size - 1
+
+  val DONE: Int = maxDepth
+  val DOWN_ACTION: Int = maxDepth - 1
+  val NEXT_ACTION: Int = -1
+  val UP_ACTION: Int = maxDepth + 1
+
+
 
   require(allVariables == variableOrdering.toSet,
     s"The set of all variables in the relationships needs to equal the variable ordering. All variables: $allVariables, variableOrdering: $variableOrdering"
@@ -28,19 +30,19 @@ class LeapfrogTriejoin(trieIterators: Map[EdgeRelationship, TrieIterator], varia
     "Variable ordering differs for some relationships."
   )
 
-  private[this] val leapfrogJoins: Array[LeapfrogJoin] = variableOrdering
+  val leapfrogJoins: Array[LeapfrogJoin] = variableOrdering
     .map(v =>
       new LeapfrogJoin(trieIterators
         .filter({ case (r, _) => r.variables.contains(v) }).values.toArray))
     .toArray
 
-  private[this] val variable2TrieIterators: Array[Array[TrieIterator]] = variableOrdering
+  val variable2TrieIterators: Array[Array[TrieIterator]] = variableOrdering
     .map( v =>
       trieIterators.filter( { case (r, _) => r.variables.contains(v)}).values.toArray
     ).toArray
 
-  private[this] var depth = -1
-  private[this] var bindings = Array.fill(allVariables.size)(-1)
+  var depth = -1
+  var bindings = Array.fill(allVariables.size)(-1)
   var atEnd = trieIterators.values.exists(i => i.atEnd)  // Assumes connected join?
 
   if (!atEnd) {
@@ -73,21 +75,16 @@ class LeapfrogTriejoin(trieIterators: Map[EdgeRelationship, TrieIterator], varia
 
 
     while (action != DONE) {
-      if (action == DOWN_ACTION) {
+      if (action <= DOWN_ACTION) {
         triejoinOpen()
-//        leapfrogDistinctNext()
+        //        leapfrogDistinctNext()
         if (currentLeapfrogJoin.atEnd) {
           action = UP_ACTION
         } else {
           bindings(depth) = currentLeapfrogJoin.key
-
-          if (depth == maxDepth) {
-            action = DONE
-          } else {
-            action = DOWN_ACTION
-          }
+          action = depth
         }
-      } else if (action == UP_ACTION) {
+      } else if (action >= UP_ACTION) {
         if (depth == 0) {
           action = DONE
           atEnd = true
@@ -111,11 +108,7 @@ class LeapfrogTriejoin(trieIterators: Map[EdgeRelationship, TrieIterator], varia
       UP_ACTION
     } else {
       bindings(depth) = currentLeapfrogJoin.key
-      if (depth == maxDepth) {
-        DONE
-      } else {
-        DOWN_ACTION
-      }
+      depth
     }
 
   }
